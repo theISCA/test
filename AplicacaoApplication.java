1c6107c24554577329915662e5a149ee9055dd39
Gabriel Escareli <gabriel.escareli@gmail.com>
2022-04-23 10:57:13 -0300

    Add files via upload

---
+++ AplicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.uniara.aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoApplication.class, args);
+	}
+
+}
